f9407a9b3da35689b371145378af9c6a90c90f62
caihongzhiyue <1909772352@qq.com>
2016-11-04 21:52:01 +0800

    food

---
+++ baidu-api/infodetail.php
@@ -0,0 +1,24 @@
+<?php
+    //如果出现乱码，从四个方面着手：文件  页面  请求  地址
+    header("Access-Control-Allow-Origin:*");
+    header('Content-type:text/html;charset=utf-8');//加入header
+
+    $id = $_GET["id"]?$_GET["id"]:0;
+    
+    $ch = curl_init();
+    $url = 'http://apis.baidu.com/tngou/info/show?id='.$id;
+    $header = array(
+        'apikey: 4987dfa8c4d230693ffefc86e1f48632',
+    );
+    // 添加apikey到header
+    curl_setopt($ch, CURLOPT_HTTPHEADER  , $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // 执行HTTP请求
+    curl_setopt($ch , CURLOPT_URL , $url);
+    $res = curl_exec($ch);
+
+   // var_dump(json_decode($res));  //类似一个js中congsole.table(object)
+
+    $result = json_decode($res,true);//json_decode见对象转换
+    echo json_encode($result);//echo是内容输出，将$result这个对象返回json对象
+?>
---
+++ baidu-api/knowledge.php
@@ -0,0 +1,21 @@
+<?php
+    header("Access-Control-Allow-Origin:*");
+    header('Content-type:text/html;charset=utf-8');//加入header
+    
+    $ch = curl_init();   
+    $url = 'http://apis.baidu.com/tngou/lore/news?classify=0&id=0&rows=20';
+    $header = array(
+        'apikey: 4987dfa8c4d230693ffefc86e1f48632',
+    );
+    // 添加apikey到header
+    curl_setopt($ch, CURLOPT_HTTPHEADER  , $header);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    // 执行HTTP请求
+    curl_setopt($ch , CURLOPT_URL , $url);
+    @$res = curl_exec($ch);
+
+   // var_dump(json_decode($res));  //类似一个js中congsole.table(object)
+
+    $result = json_decode($res,true);//json_decode见对象转换
+    echo json_encode($result);//echo是内容输出，将$result这个对象返回json对象
+?>
